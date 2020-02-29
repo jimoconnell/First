ec05308334d4ca67ef06c6d37d4f068fd71e3ee5
Jim O'Connell <jim@mmdc.net>
2020-02-29 17:05:52 -0500

    Got it working, sorta

--- First.py
+++ First.py
@@ -1,28 +1,52 @@
-# Here is a comment, to demonstrate Pull Requests
-import time
-def one_three(question):
-    answer = input(question + ": ").lower().strip()
+# # Here is a comment, to demonstrate Pull Requests
+# import time
+# def one_three(question):
+#     answer = input(question)
+#     print("")
+#     while not(answer == "2" or \
+#     answer == "1"):
+#         print("Input 1-3 please")
+#         answer = input(question + ": ")
+#         print("")
+#     if answer[0] == "1":
+#         return True
+#     else:
+#         return False
+
+# if one_three("1"):
+#      print("Initiating Mad Libs 1")
+#      time.sleep(2)
+#      print("initiated")
+#      time.sleep(.5)
+#      mad1()
+# else:
+#     print("Initiating Mad Libs 2")
+#     time.sleep(2)
+#     print("initiated")
+#     time.sleep(.5)
+#     mad2()
+#     exit()
+def yes_or_no(question):
+    answer = input(question + "(1-3): ")
     print("")
-    while not(answer == "2" or \
-    answer == "1"):
-        print("Input 1-3 please")
-        answer = input(question + ": ").lower().strip()
+    while not(answer == 1  or answer == 2 or answer == 3):
+        print("Input a number, 1-3")
+        answer = input(question + "(1 - 3):")
         print("")
-    if answer[0] == "1":
+    if answer == 1:
+        print("You typed a 1")
         return True
+    if answer == 2:
+        print("You typed a 2")
+        return True
+    if answer == 3:
+        print("You typed a 3.  Pfft. 3 is for LOOSERS. :-p")
+        return True                
     else:
         return False
-
-if one_three("Pick 1-3"):
-     print("Initiating Mad Libs 1")
-     time.sleep(2)
-     print("initiated")
-     time.sleep(.5)
-     mad1()
+#print(args.echo)
+if yes_or_no("This part of the code is the text of the question"):
+    print("Excellent! :D")
 else:
-    print("Initiating Mad Libs 2")
-    time.sleep(2)
-    print("initiated")
-    time.sleep(.5)
-    mad2()
+    print(bcolors.FAIL + "Operation cancelled. \nNo mail sent!\n ")
     exit()
